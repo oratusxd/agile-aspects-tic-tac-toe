81ed6517246c00d5d874e7285cc15ce92417d492
Utaro Borges <76760616+oratusxd@users.noreply.github.com>
2023-09-29 22:15:29 -0300

    Create resolucao.js
    Falta o teste

---
+++ bin/resolucao.js
@@ -0,0 +1,73 @@
+import readline from "readline";
+
+class Usuario {
+    constructor() {
+        this.numeros = Array(9).fill(undefined);
+        this.rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        this.contador = 0;
+        this.jogador = "X";
+        this.Njogadas = [];
+        this.rodar();
+    }
+
+    rodar() {
+        // Listen for user input using the 'line' event
+        this.rl.on("line", (input) => {
+            // Verifique se o input é um número de 0 a 8 e se a célula está vazia
+            const posicao = parseInt(input);
+            if (!isNaN(posicao) && posicao >= 0 && posicao <= 8 && this.numeros[posicao] === undefined) {
+                this.Njogadas.push(Number(input));
+                this.numeros[posicao] = this.jogador;
+                this.contador++;
+                this.exibirTabuleiro();
+
+                // Alternar entre jogador X e jogador O
+                this.jogador = this.jogador === "X" ? "O" : "X";
+
+                // Verificar a vitória ou empate
+                if (this.contador >= 5 && this.verificarVitoria()) {
+                    console.log(`Jogador ${this.jogador === "X" ? "O" : "X"} Venceu!!, Zé fini`);
+                    this.rl.close();
+                } else if (this.contador === 9) {
+                    console.log("Empate!");
+                    this.rl.close();
+                }
+            } else {
+                console.log("Jogada inválida. Tente novamente.");
+            }
+        });
+    }
+
+    exibirTabuleiro() {
+        console.log(`
+            ${this.numeros[0] || ' '} | ${this.numeros[1] || ' '} | ${this.numeros[2] || ' '}
+            ---------
+            ${this.numeros[3] || ' '} | ${this.numeros[4] || ' '} | ${this.numeros[5] || ' '}
+            ---------
+            ${this.numeros[6] || ' '} | ${this.numeros[7] || ' '} | ${this.numeros[8] || ' '}
+        `);
+    }
+
+    verificarVitoria() {
+        // Implemente a lógica de verificação de vitória aqui
+        const vitriaLinhas = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Linhas
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Colunas
+            [0, 4, 8], [2, 4, 6] // Diagonais
+        ];
+
+        for (const linha of vitriaLinhas) {
+            const [a, b, c] = linha;
+            if (this.numeros[a] && this.numeros[a] === this.numeros[b] && this.numeros[a] === this.numeros[c]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+const usuarioInstancia = new Usuario();
